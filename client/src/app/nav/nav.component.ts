d5fd4b179c464733d7d9825225e1f406565eb3bd
Ronak <random07use@gmail.com>
2021-08-03 19:12:14 +0530

    Routing, AuthGuard added

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { THIS_EXPR, ThrowStmt } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/User';
 import { AccountService } from '../_services/account.service';
@@ -13,20 +15,24 @@ export class NavComponent implements OnInit {
   
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, 
+              private route: Router,
+              private toastrService: ToastrService) { }
 
   ngOnInit(): void {
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.route.navigateByUrl('/members');
     }, error => {
       console.log(error);
+      this.toastrService.error(error.error);
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.route.navigateByUrl('/');
   }
 }
